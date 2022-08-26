45fc75fa840bc61a9fe44ba7415cd836b5e32227
Vansh-1419 <vanshtandon1423@gmail.com>
2022-08-26 23:12:18 +0530

    First Update

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+import img from "./image.jpg";
+function App() {
+  return (
+    <div className="App">
+      <img src={img} className="main" />
+    </div>
+  );
+}
+
+export default App;
